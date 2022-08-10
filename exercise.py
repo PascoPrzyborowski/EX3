2a9c7f5b2031cd0d9f9e22798c58fa1c40890206
PascoPrzyborowski <unity01@gmx.net>
2022-08-10 16:16:38 +0200

    Ex 3

--- exercise.py
+++ exercise.py
@@ -1,3 +1,58 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+
+
+
+# Task1
+
+def even_or_odd(number):
+    #print(number)
+    if number %2 !=  0:
+        return 'odd number'
+    else:
+        return 'even Number'    
+
+
+def user_input():
+    counter = 0
+    while counter < 3:
+        number = int(input('Enter some Numbers: '))
+        counter += 1
+        print(even_or_odd(number))
+        
+
+
+    
+#if __name__ == "__main__":
+user_input()
+
+def Test():
+    return 'Pasco'
+
+print(Test())
+
+
+#Task 2
+n = int(input("How many Arguments wanna do?:"))
+Start = int(input("Startnumber please?:"))
+Stop = int(input("Stopnumber please?:"))
+Step = int(input("Step please?:"))
+
+for i in range(Start,Stop,Step):
+    print(i)
+
+
+# Task 3
+
+
+
+# Task 4
+
+
+
+# Task 5
+
+
+
+# Task 6
